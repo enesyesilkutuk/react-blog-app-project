04d5cb478f02ba751070d158307fcbdc94bd7dcf
enesyesilkutuk <enesyesilkutuk@gmail.com>
2022-04-28 14:25:46 +0300

    bug fixed

---
+++ src/pages/Login.js
@@ -0,0 +1,173 @@
+import { LoginOutlined } from "@mui/icons-material";
+import { Avatar, Button, Container, Grid, Link, TextField, Typography } from "@mui/material";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { Formik, useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import googleIcon from "../assets/gfavicon.png";
+import { auth } from "../auth/firebase-config";
+
+const Schema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Email is required"),
+  password: Yup.string()
+    .required("No password provided")
+    .min(8, "Password is too short - should be 8 chars minimum")
+    .matches(/\d+/, "Password must have a number")
+    .matches(/[a-z]+/, "Password must have a lowercase")
+    .matches(/[A-Z]+/, "Password must have a uppercase")
+    .matches(/[!?.@#$%^&*()-+]+/, "Password must have a special char"),
+});
+
+const Login = () => {
+  const navigate = useNavigate()
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try{
+      await signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user
+        console.log(user)
+        navigate("/")
+      }).catch((err) => {
+        alert(err.message)
+      })
+    }catch(err) {alert(err.message)}
+  }
+
+  const formik = useFormik({
+    initialValues:{
+      email:"",
+      password:""
+    },
+    validationSchema:Schema,
+    onSubmit: async (values) => {
+      try{
+        await signInWithEmailAndPassword(auth, values.email, values.password)
+      .then((result) => {
+        const user = result.user
+        console.log(user)
+        navigate("/")
+      })
+      }catch(err){
+        alert(err.message)}
+      
+    }
+  })
+
+  return (
+    <Container
+      sx={{
+        marginTop: "2rem",
+        height: "auto",
+        bgcolor: "background.paper",
+        boxShadow: 1,
+        borderRadius: 2,
+        textAlign: "center",
+        padding: "1rem",
+      }}
+      maxWidth="sm"
+    >
+      <Avatar
+        sx={{
+          margin: "1rem auto",
+          bgcolor: "#4CAF50",
+        }}
+      >
+        <LoginOutlined sx={{ backgroundColor: "#4CAF50" }} />
+      </Avatar>
+
+      <Typography sx={{ margin: "2rem" }} variant="h4">
+        Login
+      </Typography>
+      <Formik>
+        {() => (
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={5}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  helperText={formik.touched.email && formik.errors.email}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  type="password"
+                  name="password"
+                  label="Password"
+                  variant="outlined"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  helperText={formik.touched.password && formik.errors.password}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  sx={{ backgroundColor: "#4CAF50", marginBottom: "1rem" }}
+                  fullWidth
+                  item
+                  variant="contained"
+                  type="submit"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{
+                    backgroundColor: "#fff",
+                    marginBottom: "1rem",
+                    color: "gray",
+                    fontWeight:"bold",
+                    ":hover": {
+                      bgcolor: "gray",
+                      color: "white",
+                    },
+                  }}
+                  fullWidth
+                  item
+                  variant="contained"
+                  onClick={signInWithGoogle}
+                >
+                  <img
+                    style={{ width: "1rem", marginRight: "1rem" }}
+                    src={googleIcon}
+                    alt="googleIcon"
+                  />
+                  Sign in with Google
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
+      <p style={{cursor:"pointer"}} onClick={() => navigate("/register")}>
+        Or
+        <Link
+          sx={{
+            textDecoration: "none",
+            fontWeight: "600",
+            paddingLeft: "0.5rem",
+            cursor: "pointer",
+            color: "#24292E",
+          }}
+          onClick={() => navigate("/register")}
+        >
+          Register with email.
+        </Link>
+      </p>
+    </Container>
+  )
+}
+
+export default Login
---
+++ src/pages/Register.js
@@ -0,0 +1,206 @@
+import { PersonAddAltOutlined } from "@mui/icons-material";
+import {
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
+import { Formik, useFormik } from "formik";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import googleIcon from "../assets/gfavicon.png";
+import { auth } from "../auth/firebase-config"
+
+const Schema = Yup.object().shape({
+  username: Yup.string()
+    .required("Display name is required")
+    .min(2, "Too short")
+    .max(15, "Must be 15 char or less"),
+  email: Yup.string().email("Invalid Email").required("Email is required"),
+  password: Yup.string()
+    .required("No password provided")
+    .min(8, "Password is too short - should be 8 chars minimum")
+    .matches(/\d+/, "Password must have a number")
+    .matches(/[a-z]+/, "Password must have a lowercase")
+    .matches(/[A-Z]+/, "Password must have a uppercase")
+    .matches(/[!?.@#$%^&*()-+]+/, "Password must have a special char"),
+  confirm: Yup.string()
+    .required("No password provided")
+    .min(8, "Password is too short - should be 8 chars minimum")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
+
+const Register = () => {
+  
+  const navigate = useNavigate();
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try{
+      await signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user
+        console.log(user)
+        navigate("/")
+      }).catch((err) => {
+        alert(err.message)
+      })
+    }catch(err) {alert(err.message)}
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirm: ""
+    },
+    validationSchema: Schema,
+    onSubmit: async (values) => {
+      try{
+        let user = await createUserWithEmailAndPassword(auth, values.email, values.password)
+        console.log(user)
+        await updateProfile(auth.currentUser, {displayName:values.username})
+        //console.log(auth.currentUser)
+        navigate("/")
+      }catch(err){alert(err.message)}
+    }
+  });
+
+  return (
+    <Container
+      sx={{
+        marginTop: "2rem",
+        marginBottom:"2rem",
+        height: "auto",
+        bgcolor: "background.paper",
+        boxShadow: 1,
+        borderRadius: 2,
+        textAlign: "center",
+        padding: "1rem",
+      }}
+      maxWidth="sm"
+    >
+      <Avatar
+        sx={{
+          margin: "1rem auto",
+          bgcolor: "#4CAF50",
+        }}
+      >
+        <PersonAddAltOutlined />
+      </Avatar>
+      <Typography sx={{ margin: "2rem" }} variant="h4">
+        Register
+      </Typography>
+      <Formik>
+        {() => (
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  name="username"
+                  label="User Name"
+                  variant="outlined"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  helperText={formik.touched.username && formik.errors.username}
+                  error={
+                    formik.touched.username && Boolean(formik.errors.username)
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  helperText={formik.touched.email && formik.errors.email}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  type="password"
+                  name="password"
+                  label="Password"
+                  variant="outlined"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  helperText={formik.touched.password && formik.errors.password}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  type="password"
+                  name="confirm"
+                  label="Confirm"
+                  variant="outlined"
+                  value={formik.values.confirm}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  helperText={formik.touched.confirm && formik.errors.confirm}
+                  error={
+                    formik.touched.confirm && Boolean(formik.errors.confirm)
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  sx={{ backgroundColor: "#4CAF50" }}
+                  fullWidth
+                  item
+                  variant="contained"
+                  type="submit"
+                >
+                  Register
+                </Button>
+                <Button
+                  sx={{
+                    marginTop: "0.5rem",
+                    backgroundColor: "#fff",
+                    marginBottom: "1rem",
+                    color: "gray",
+                    fontWeight:"bold",
+                    ":hover": {
+                      bgcolor: "gray",
+                      color: "white",
+                    },
+                  }}
+                  fullWidth
+                  item
+                  variant="contained"
+                  onClick={signInWithGoogle}
+                >
+                  <img
+                    style={{ width: "1rem", marginRight: "1rem" }}
+                    src={googleIcon}
+                    alt="googleIcon"
+                  />
+                  Sign in with Google
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
+
+export default Register;
