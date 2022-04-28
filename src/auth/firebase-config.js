a321f9756584c64b6994c9d938c772a6f41b51f4
enesyesilkutuk <enesyesilkutuk@gmail.com>
2022-04-28 13:06:42 +0300

    bug fixed

--- src/auth/firebase-config.js
+++ src/auth/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyALMBxkkvi3eq0mYDF1E4T1IysAicJygtw",
-  authDomain: "blog-app-milestone-f4a2b.firebaseapp.com",
-  projectId: "blog-app-milestone-f4a2b",
-  storageBucket: "blog-app-milestone-f4a2b.appspot.com",
-  messagingSenderId: "161399597206",
-  appId: "1:161399597206:web:c607da017e0310933e3ab7",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
